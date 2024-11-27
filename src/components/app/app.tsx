40effc86a1850218e953802d1bbd495b22242f6c
Moon Studico <moonstudico@gmail.com>
2024-11-27 11:18:37 +0300

    added routes and layout

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,11 @@
+import { Route, BrowserRouter, Routes} from 'react-router-dom';
 import MainPage from '../../pages/main-page/main-page';
+import Favorites from '../../pages/favorites/favorites';
+import Login from '../../pages/login/login';
+import Offer from '../../pages/offer/offer';
+import { AppRoute } from '../const';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import Layout from '../layout/layout';
 
 type Props = {
   countOffers: number;
@@ -6,7 +13,33 @@ type Props = {
 
 function App({countOffers}:Props): JSX.Element{
   return(
-    <MainPage countOffers={countOffers}/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />} >
+          <Route
+            index
+            path = {AppRoute.Main}
+            element = {<MainPage countOffers={countOffers}/>}
+          />
+          <Route
+            path = {AppRoute.Login}
+            element = {<Login />}
+          />
+          <Route
+            path = {AppRoute.Favorites}
+            element = {<Favorites/>}
+          />
+          <Route
+            path = {AppRoute.Offer}
+            element = {<Offer/>}
+          />
+          <Route
+            path = "*"
+            element = {<NotFoundPage/>}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
